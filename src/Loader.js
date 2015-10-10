bdf23ba2eb70f2e62fd88292df96e9cd6b9bc093
nils <nils.rietveld@boostermedia.com>
2015-10-10 15:24:11 +0200

    created basic ugly component system created player without physics or collisions, controls will be updated

--- src/Loader.js
+++ src/Loader.js
@@ -13,6 +13,8 @@ define([], function ()
         game.load.tilemap('level1', 'assets/levels/level1.json', null, Phaser.Tilemap.TILED_JSON);
 
         game.load.image('gradiented', 'assets/tiles/gradiented-sheet.png');
+
+        game.load.image('player', 'assets/characters/player.png');
     };
 
     return Loader;
